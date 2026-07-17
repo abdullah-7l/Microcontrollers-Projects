7fcc0150da0faa7bcdb669d94bca3162f5043113
abdullah alrefaei <abodi5746@icloud.com>
2026-07-17 03:01:00 +0300

    Add servo sweep and hold functionality

---
+++ Arduino/4_Servo_Sweep_Hold/4_Servo_Sweep_Hold.ino
@@ -0,0 +1,53 @@
+تفضل، هذا الكود نظيف وجاهز للرفع على GitHub بدون أي تعليقات:
+
+```cpp
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+void setup() {
+  servo1.attach(3);
+  servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(9);
+
+  unsigned long startTime = millis();
+
+  while (millis() - startTime < 2000) {
+
+    for (int pos = 0; pos <= 180; pos += 1) {
+      if (millis() - startTime >= 2000) break; 
+      
+      servo1.write(pos);
+      servo2.write(pos);
+      servo3.write(pos);
+      servo4.write(pos);
+      
+      delay(5); 
+    }
+
+    for (int pos = 180; pos >= 0; pos -= 1) {
+      if (millis() - startTime >= 2000) break; 
+      
+      servo1.write(pos);
+      servo2.write(pos);
+      servo3.write(pos);
+      servo4.write(pos);
+      
+      delay(5);
+    }
+  }
+
+  servo1.write(90);
+  servo2.write(90);
+  servo3.write(90);
+  servo4.write(90);
+}
+
+void loop() {
+}
+
+```
